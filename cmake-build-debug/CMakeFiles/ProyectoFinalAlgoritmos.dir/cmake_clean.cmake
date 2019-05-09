1a19613e988428352a25c445e65471c4e6d67a9d
Salo Charabati <salochara@gmail.com>
2019-05-08 19:41:37 -0500

    initial commit

---
+++ cmake-build-debug/CMakeFiles/ProyectoFinalAlgoritmos.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProyectoFinalAlgoritmos.dir/main.cpp.o"
+  "ProyectoFinalAlgoritmos.pdb"
+  "ProyectoFinalAlgoritmos"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProyectoFinalAlgoritmos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
